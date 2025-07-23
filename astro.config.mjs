5ad68886078c401be64767c6e422a529d1e8ecc0
Srinivas Dasyam <dahasyam@gmail.com>
2025-07-23 17:49:17 +0530

    Added draft functionlaity

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import remarkGfm from 'remark-gfm';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap(), react()],
+	markdown: {
+		remarkPlugins: [remarkGfm],},
 });
